919cf50ea8c42e339d69f2571490548c51cafbfb
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-11-09 20:47:15 -0200

    Fixed invalid letter handling

--- app.rb
+++ app.rb
@@ -37,17 +37,23 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
-    if @game.guesses.index(letter) != nil || @game.wrong_guesses.index(letter) != nil
-      flash[:message] = 'You have already used that letter'
+    begin
+      letter = params[:guess].to_s[0]
+      if @game.guesses.index(letter) != nil || @game.wrong_guesses.index(letter) != nil
+        flash[:message] = 'You have already used that letter'
+        redirect '/show'
+      end
+      @game.guess(letter)
+      action = @game.check_win_or_lose
+      logger.info "Action: #{action}"
+      redirect '/win'  if action == :win
+      redirect '/lose' if action == :lose
+      redirect '/show' if action == :play
+    rescue ArgumentError, TypeError
+      logger.info 'Empty or invalid letter'
+      flash[:message] = 'Empty or invalid letter'
       redirect '/show'
     end
-    @game.guess(letter)
-    action = @game.check_win_or_lose
-    logger.info "Action: #{action}"
-    redirect '/win'  if action == :win
-    redirect '/lose' if action == :lose
-    redirect '/show' if action == :play
   end
 
   # Everytime a guess is made, we should eventually end up at this route.
